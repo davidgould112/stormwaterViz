de94549838d4beb55ae3fc1c87c547631a369bec
dagould <dagould@allegisgroup.com>
2021-08-13 14:14:52 -0500

    8/13 build

---
+++ build/precache-manifest.107a53f3044eb81c50e62eb808734b6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ac8df57df2ca3ff67a464219359c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b2f850868ebfa21e4e2",
+    "url": "/static/css/main.df10a3bb.chunk.css"
+  },
+  {
+    "revision": "e0dc7d6e7ca61c1a03f1",
+    "url": "/static/js/2.f29e449f.chunk.js"
+  },
+  {
+    "revision": "97af2597212b028677ebd4da1feac364",
+    "url": "/static/js/2.f29e449f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2f850868ebfa21e4e2",
+    "url": "/static/js/main.be1ae22b.chunk.js"
+  },
+  {
+    "revision": "a7c9f49c07e13b14d8c9",
+    "url": "/static/js/runtime-main.4faf4e71.js"
+  },
+  {
+    "revision": "1afa1e20f6d21affdac3a44eb81774da",
+    "url": "/static/media/cig-logo.1afa1e20.png"
+  }
+]);
